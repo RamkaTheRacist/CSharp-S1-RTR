a2dd0bda3e11ec7f7fa63ecc908183179baba64d
Ramka <whereisourgod@gmail.com>
2022-04-27 10:15:00 +0300

    task6

--- HW/task6/Program.cs
+++ HW/task6/Program.cs
@@ -1,1 +1,20 @@
 ﻿//Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+Console.Writelint("Number?");
+int number = int.Parse(Console.ReadLine());
+int digit1 = number / 10;
+int digit2 = number % 10;
+if(number >= 10 && number <= 99)
+{
+    if(digit1 > digit2)
+    {
+        Console.WriteLine($"Max digit = {digit1}, Min digit = {digit2}");
+    }
+    else
+    {
+        Console.WriteLine($"Max digit = {digit2}, Min digit = {digit1}");
+    }
+}
+else
+{
+    Console.WriteLine("Wrong input data");
+}
