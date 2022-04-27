1d1b2bd056e3fbdd2d0bcf7799c86bd27cf80e80
Ramka <whereisourgod@gmail.com>
2022-04-27 09:33:09 +0300

    task1

--- HW/task1/Program.cs
+++ HW/task1/Program.cs
@@ -1,1 +1,11 @@
 ﻿//По двум заданным числам проверять является ли первое квадратом второго
+int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+if((n2 * n2) == n1)
+{
+    Console.WriteLine($"Yes, {n1} is square of {n2}");
+}
+else
+{
+    Console.WriteLine($"No, {n1} isn`t square of {n2}");
+}
