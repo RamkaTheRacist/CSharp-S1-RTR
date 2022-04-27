3cb3cc109409c0a9772ecd671d5d49f2e736dafe
Ramka <whereisourgod@gmail.com>
2022-04-27 07:32:21 +0300

    obj3

---
+++ obj3/Program.cs
@@ -0,0 +1,15 @@
+﻿//Найти максимальное из трех чисел
+int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+int n3 = int.Parse(Console.ReadLine());
+int max = n1;
+if(n2 > n1)
+{
+    max = n2;
+}
+if(max < n3)
+{
+    max = n3;
+}
+Console.Write("max = ");
+Console.Write(max);
