f34f96d33e754ff550f57e600bccb73d770bf244
Ramka <whereisourgod@gmail.com>
2022-04-27 07:47:19 +0300

    obj4

---
+++ obj4/Program.cs
@@ -0,0 +1,9 @@
+﻿//Показать числа от -N до N
+double n = double.Parse(Console.ReadLine());
+double nmax = n;
+double nmin = -n;
+while(nmin <= nmax)
+{
+    Console.WriteLine(nmin);
+    nmin = nmin + 1;
+}
