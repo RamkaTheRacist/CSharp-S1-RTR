2dcf016fe3faaa86000e28def14588bae7576f74
Ramka <whereisourgod@gmail.com>
2022-04-27 10:32:06 +0300

    task8

--- HW/task8/Program.cs
+++ HW/task8/Program.cs
@@ -1,1 +1,14 @@
 ﻿//Выяснить, кратно ли число заданному, если нет, вывести остаток
+Console.WriteLine("Number?");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("On what will divibe by?");
+int number1 = int.Parse(Console.ReadLine());
+if(number % number1 == 0)
+{
+    Console.WriteLine($"{number} is multiple of {number1}");
+}
+else
+{
+    int tmp = number % number1;
+    Console.WriteLine($"{tmp} is remainder of {number} divibed by {number1}");
+}
