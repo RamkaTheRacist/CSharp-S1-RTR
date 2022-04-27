d93c7517ed35c3cf8773002616c7b04e3f3205a4
Ramka <whereisourgod@gmail.com>
2022-04-27 10:02:16 +0300

    task4

--- HW/task4/Program.cs
+++ HW/task4/Program.cs
@@ -1,1 +1,33 @@
 ﻿//Показать четные числа от 1 до N
+Console.WriteLine("Number?");
+int number = int.Parse(Console.ReadLine());
+int tmp = 1;
+if(number == 1)
+{
+    Console.Write("Nothing here");
+}
+else
+{
+ if(number > 1) 
+ {
+    while(number >= tmp)
+    {
+        if(tmp % 2 == 0)
+        {
+        Console.Write(" "+tmp);
+        }
+    tmp++ ;
+    }
+ }
+ else
+ {
+    while(tmp >= number)
+    {
+        if(tmp % 2 == 0)
+        {
+        Console.Write(" "+tmp);
+        }
+    tmp-- ;
+    }
+ }
+}
