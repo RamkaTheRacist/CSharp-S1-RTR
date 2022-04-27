9df529ade48649aed22c1023b79034ffe3ed547f
Ramka <whereisourgod@gmail.com>
2022-04-27 09:43:43 +0300

    task3

--- HW/task3/Program.cs
+++ HW/task3/Program.cs
@@ -1,1 +1,11 @@
 ﻿//Выяснить является ли число чётным
+Console.WriteLine("Which number?");
+int number = int.Parse(Console.ReadLine());
+if(number % 2 == 0)
+{
+    Console.Write("Even number");
+}
+else
+{
+    Console.Write("Odd number");
+}
