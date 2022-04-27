34e0d3b3ef043f08e940faefae0a2c16c8c831ff
Ramka <whereisourgod@gmail.com>
2022-04-27 07:32:46 +0300

    fixed

---
+++ obj1/Program.cs
@@ -0,0 +1,2 @@
+﻿int number = int.Parse(Console.ReadLine()); //Для чисел
+Console.Write(number * number);
---
+++ obj2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Даны два числа. Показать большее и меньшее число
+int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+if(n1 > n2)
+{
+    Console.Write("Max = ");
+    Console.WriteLine(n1);
+    Console.Write("Min = ");
+    Console.Write(n2);
+}
+else
+{
+    Console.Write("Max = ");
+    Console.WriteLine(n2);
+    Console.Write("Min = ");
+    Console.Write(n1);
+}
