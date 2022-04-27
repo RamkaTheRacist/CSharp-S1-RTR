a43bd41956a80a265c258041a3d21bff6b14abdf
Ramka <whereisourgod@gmail.com>
2022-04-27 09:38:19 +0300

    task2

--- HW/task2/Program.cs
+++ HW/task2/Program.cs
@@ -1,1 +1,35 @@
 ﻿//По заданному номеру дня недели вывести его название
+Console.WriteLine("Which day of week?");
+int digit = int.Parse(Console.ReadLine());
+if(digit == 1)
+{
+    Console.WriteLine("Monday");
+}
+if(digit == 2)
+{
+    Console.WriteLine("Tuesday");
+}
+if(digit == 3)
+{
+    Console.WriteLine("Wednesday");
+}
+if(digit == 4)
+{
+    Console.WriteLine("Thursday");
+}
+if(digit == 5)
+{
+    Console.WriteLine("Friday");
+}
+if(digit == 6)
+{
+    Console.WriteLine("Saturday");
+}
+if(digit == 7)
+{
+    Console.WriteLine("Sunday");
+}
+else
+{
+    Console.WriteLine("Are u dumb?");
+}
