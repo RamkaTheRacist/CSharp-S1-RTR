d711db710844690b36690e9e4dcc04d66100c3df
Ramka <whereisourgod@gmail.com>
2022-04-27 10:07:25 +0300

    task5

--- HW/task5/Program.cs
+++ HW/task5/Program.cs
@@ -1,1 +1,6 @@
 ﻿//Показать вторую цифру трёхзначного числа
+Console.WriteLine("Number?");
+int number = int.Parse(Console.ReadLine());
+int tmp = number / 10;
+int SecondDigit = tmp % 10;
+Console.WriteLine($"Second digit of {number} is {SecondDigit}");
